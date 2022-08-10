39bc0ca26afc0a1379dc5df5d6b67771de1b5917
Igopk <101907629+Igopk@users.noreply.github.com>
2022-08-10 08:23:04 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-admin = 'nuevoss01'
-bot_token = "5472171484:AAFBCkUR2jTz2uoPrxfNUGKLzNEUsXbJEzg"
+admin = 'nautaii'
+bot_token = "5442203153:AAF7PbX1qVcOkHQLToirfNLq_IklTkLr2T8"
 
-groupid= -684078191
+groupid= -1001737219562
